636e6e8908ac844af450476df8443cc05bf0b1ef
guigomes91 <guilherme.gomes91@outlook.com>
2025-02-02 20:46:32 -0300

    Configuracao kafka e banco

--- src/main/java/org/br/mineradora/message/KafkaEvent.java
+++ src/main/java/org/br/mineradora/message/KafkaEvent.java
@@ -5,6 +5,7 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import org.br.mineradora.dto.ProposalDTO;
 import org.br.mineradora.dto.QuotationDTO;
+import org.br.mineradora.service.OpportunityService;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
