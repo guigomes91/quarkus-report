f2192f8e937a9dc9d8b6292b54da5bd8b677b6d4
guigomes91 <guilherme.gomes91@outlook.com>
2025-02-05 20:20:40 -0300

    config keycloack no properties

--- src/main/java/org/br/mineradora/controller/OpportunityController.java
+++ src/main/java/org/br/mineradora/controller/OpportunityController.java
@@ -1,20 +1,28 @@
 package org.br.mineradora.controller;
 
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import org.br.mineradora.dto.OpportunityDTO;
 import org.br.mineradora.service.OpportunityService;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import java.util.List;
 
 @Path("/api/opportunity")
+@Authenticated
 public class OpportunityController {
     @Inject
     OpportunityService opportunityService;
 
+    @Inject
+    JsonWebToken jsonWebToken;
+
     @GET
     @Path("/data")
+    @RolesAllowed({"user", "manager"})
     public List<OpportunityDTO> generateReport() {
         return opportunityService.generateOpportunityData();
     }
